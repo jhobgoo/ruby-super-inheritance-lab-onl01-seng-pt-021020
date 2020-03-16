fe2d8293ba3c88a3d30a52d684de968168832790
Joel Hobgood <jhobgoo@gmail.com>
2020-03-16 03:29:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,11 @@
-class ChattyStudent < student
-  
+class Student
+
   def hello
-    super
-    puts "greeting phrase"
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+  end
+  
+  def raise_hand
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
   end
   
 end
