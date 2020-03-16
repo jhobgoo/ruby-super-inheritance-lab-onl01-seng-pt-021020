054d36d962413ae875c9a906a342a1534d0b342d
Joel Hobgood <jhobgoo@gmail.com>
2020-03-16 03:32:45 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++
@@ -1,8 +0,0 @@
-class ChattyStudent < Student
-  
-  def hello
-    super
-    puts "Hey there! I'm so excited to learn stuff.\nHoware you doing today? I'm okay, but I'm kind of tired, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
-  end
-  
-end
