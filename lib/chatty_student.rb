d815c27c97b55f73ef989d99c0aee13f58d35542
Joel Hobgood <jhobgoo@gmail.com>
2020-03-16 03:32:38 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,7 @@ class ChattyStudent < Student
   
   def hello
     super
-    puts "Hey there! I'm so excited to learn stuff.\nHoware you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    puts "Hey there! I'm so excited to learn stuff.\nHoware you doing today? I'm okay, but I'm kind of tired, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
   
 end
